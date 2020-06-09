f7a26816ccd38ccf6f87220891a422efdd4f92f1
anjanijha <anjanipremnitrr@gmail.com>
2020-06-10 01:54:34 +0530

    add Search page  feature code and add a method in Login Page code

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,4 +4,7 @@ public class LoginPage {
 	public void logiPage() {
 		System.out.println("Login Page");
 	}
+	public void loginSet() {
+		System.out.println("Login Set");
+	}
 }
--- src/test/java/com/qa/tests/LoginTest.java
+++ src/test/java/com/qa/tests/LoginTest.java
@@ -1,9 +1,6 @@
 package com.qa.tests;
 
-import org.testng.annotations.Test;
-
 public class LoginTest {
-	@Test
 	public void logiPageTset() {
 		System.out.println("Login Page Test");
 	}
